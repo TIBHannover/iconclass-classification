b80ea2cad4abf28558fec21db574a1da58416c1c
matthias springstein <matthias.springstein@tib.eu>
2020-12-14 13:49:36 +0100

    Fix logging and saving issues.

--- models/convnet_yolo_loss.py
+++ models/convnet_yolo_loss.py
@@ -66,15 +66,50 @@ class ConvnetYoloLoss(LightningModule):
         ids_vec = batch["ids_vec"]
         cls_vec = batch["cls_vec"]
         cls_ids_mask_vec = batch["cls_ids_mask_vec"]
+
+        self.image = image
+
         logits = self(image)
 
         loss = self.loss(logits, ids_vec) * cls_ids_mask_vec
         loss = torch.sum(loss) / torch.sum(cls_ids_mask_vec)
-        return loss
+        return {"loss": loss}
+
+    def training_step_end(self, outputs):
+
+        return {
+            "loss": outputs["loss"].mean(),
+            "progress_bar": {"train/loss": outputs["loss"].mean(),},
+            "log": {"train/loss": outputs["loss"].mean(),},
+        }
+
+    def validation_step(self, batch, batch_idx):
+        image = batch["image"]
+        ids_vec = batch["ids_vec"]
+        cls_vec = batch["cls_vec"]
+        cls_ids_mask_vec = batch["cls_ids_mask_vec"]
+        logits = self(image)
+
+        loss = self.loss(logits, ids_vec) * cls_ids_mask_vec
+        loss = torch.sum(loss) / torch.sum(cls_ids_mask_vec)
+        return {"loss": loss}
+
+    def validation_epoch_end(self, outputs):
+
+        loss = 0.0
+        count = 0
+        for output in outputs:
+            loss += output["loss"]
+            count += 1
+
+        return {
+            "loss": loss / count,
+            "progress_bar": {"val/loss": loss / count,},
+            "log": {"val/loss": loss / count,},
+        }
 
     def configure_optimizers(self):
-        # REQUIRED
-        # can return multiple optimizers and learning_rate schedulers
+        
         optimizer = torch.optim.Adam(
             params=list(self.net.parameters()) + list(self.classifier.parameters()) + list(self.fc.parameters()),
             lr=0.001,
